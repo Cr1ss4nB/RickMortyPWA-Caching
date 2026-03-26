92fea31e6eb5e337268387fe915ea828c6eaa0b1
Andrés Basto <cabalacode@gmail.com>
2026-03-26 00:09:13 -0500

    Fase 2 y 3: Definición de las caches y los métodos fetch usando la estrategia de cache especificadas

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,74 @@
-// Service Worker
+const CACHE_STATIC_NAME = 'static-v2';
+const CACHE_DYNAMIC_NAME = 'dynamic-v2';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+// Este método precachea el App Shell
+self.addEventListener('install', event => {
+    const cacheStatic = caches.open(CACHE_STATIC_NAME)
+        .then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './css/style.css',
+                './js/app.js',
+                './img/logo.png',
+                './pages/offline.html'
+            ]);
+        });
+    const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME)
+        .then(cache => {
+            return cache.addAll([
+                'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
+            ]);
+        });
+    event.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+    const url = event.request.url;
+    if (url.includes('rickandmortyapi.com')) {
+        event.respondWith(networkFirst(event.request));
+        return;
+    }
+    if (event.request.destination === 'image') {
+        event.respondWith(staleWhileRevalidate(event.request));
+        return;
+    }
+    event.respondWith(cacheFirst(event.request));
+});
+
+// App Shell -> Cache First
+function cacheFirst(req) {
+    return caches.match(req).then(res => {
+        if (res) return res;
+        return fetch(req);
+    });
+}
+
+// API -> Network First
+function networkFirst(req) {
+    return fetch(req)
+        .then(res => {
+            return caches.open(CACHE_DYNAMIC_NAME).then(cache => {
+                cache.put(req, res.clone());
+                return res;
+            });
+        })
+        .catch(() => {
+            return caches.match(req);
+        });
+}
+
+// Imágenes -> Stale While Revalidate
+function staleWhileRevalidate(req) {
+    return caches.match(req).then(cachedRes => {
+        const fetchPromise = fetch(req).then(networkRes => {
+            caches.open(CACHE_DYNAMIC_NAME).then(cache => {
+                cache.put(req, networkRes.clone());
+            });
+            return networkRes;
+        });
+        return cachedRes || fetchPromise;
+    });
+}
